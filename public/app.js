acd8a9d10cb1b8718ae76a1fc4081d4bc94778ba
Valeriy Harlamov <valera545@gmail.com>
2022-02-05 13:08:24 +0300

    Lesson-05 hw-2

--- public/app.js
+++ public/app.js
@@ -1,21 +1,46 @@
 document.addEventListener('click', (e) => {
 	const id = e.target.dataset.id
+	let noteInnerHtml = ''
 
 	if (e.target.dataset.type === 'remove') {
 		remove(id).then(() => {
 			e.target.closest('li').remove()
 		})
 	} else if (e.target.dataset.type === 'edit') {
-		const data = prompt(
-			'Новый текст: ',
-			e.target.closest('li').firstChild.data.trim()
-		)
+		// const data = prompt(
+		// 	'Новый текст: ',
+		// 	e.target.closest('li').firstChild.data.trim()
+		// )
+		noteInnerHtml = e.target.closest('li').innerHTML
+		const inputValue = e.target.closest('li').firstChild.data.trim()
 
 		edit(id, data).then(() => {
 			e.target.closest('li').firstChild.data = data
 		})
 	}
 })
+
+function getForm(value) {
+	return `<form action="/" method="post">
+  <input type="text" value=${value}/>
+<div class="d-flex">
+  <button
+    class="btn btn-primary me-2"
+    data-type="edit"
+    data-id=${id}
+    type="submit"
+  >
+    Обновить
+  </button>
+  <button
+    class="btn btn-danger"
+    data-type="remove"
+    data-id="<%= notes[i].id %>"
+  >
+    Отмена
+  </button>
+</div></form>`
+}
 
 async function remove(id) {
 	await fetch(`/${id}`, { method: 'DELETE' })
